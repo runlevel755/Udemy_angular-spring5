b507301f609527c7d7d5e397a0091e7102e06943
Pedro González <pedro96javier@gmail.com>
2020-06-15 17:08:53 +0200

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# Udemy_angular-spring5
-This repository contains the source code learned in this [course](https://www.udemy.com/share/1020J6A0obcVlRQnw=/).
--- spring-boot-backend-apirest/src/main/java/com/runlevel/springboot/backend/apirest/dao/IClientDao.java
+++
@@ -1,9 +0,0 @@
-package com.runlevel.springboot.backend.apirest.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.runlevel.springboot.backend.apirest.entity.Client;
-
-public interface IClientDao extends JpaRepository<Client, Long> {
-
-}
